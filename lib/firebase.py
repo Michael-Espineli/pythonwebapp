4d47eb3ff84df43a8eeac4ab4810904ff09f9b77
Michael-Espineli <153308196+Michael-Espineli@users.noreply.github.com>
2024-10-21 11:44:05 -0700

    Update firebase.py

--- lib/firebase.py
+++ lib/firebase.py
@@ -1,2 +1,29 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize the Firebase Admin SDK
+
+
 def my_function()->str:
-  return "Hello from a function"
+    cred = credentials.Certificate('key.json')  # Path to your service account key
+    firebase_admin.initialize_app(cred)
+
+# Create a Firestore client
+    db = firestore.client()
+    # Get a document
+    #doc_ref = db.collection('users').document('user1')
+    #doc = doc_ref.get()
+
+    #if doc.exists:
+        #print(f'Document data: {doc.to_dict()}')
+    #else:
+    #print('No such document!')
+
+# Query data
+    users_ref = db.collection('users')
+    query = users_ref.stream()
+
+    #for doc in query:
+        #print(f'{doc.id} => {doc.to_dict()}')
+    val = "Hello from a function" + str(len(query))
+    return val
