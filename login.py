4e087a74ea767a25ae779cd9cb6dad1454e38146
Michael-Espineli <153308196+Michael-Espineli@users.noreply.github.com>
2024-10-20 23:11:29 -0700

    Update login.py

--- login.py
+++ login.py
@@ -4,23 +4,8 @@ from types import FrameType
 from flask import Flask, request, jsonify
 from utils.logging import logger
 import os
-from firebase_admin import credentials, firestore, initialize_app
-# Initialize Firestore DB
-cred = credentials.Certificate('key.json')
-default_app = initialize_app(cred)
-db = firestore.client()
-todo_ref = db.collection('toDoTesters')
 
-@app.route('/signUp')
-def signUp():
-    """
-        create() : Add document to Firestore collection with request body
-        Ensure you pass a custom ID as part of json body in post request
-        e.g. json={'id': '1', 'title': 'Write a blog post'}
-    """
-    try:
-        id = request.json['id']
-        todo_ref.document(id).set(request.json)
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An Error Occured: {e}"
+@app.route('/login')
+def login():
+
+        return "Hello Login.py file"
