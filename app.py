95879ed1baebdc75da22afd4d42905e86c5f9968
Michael-Espineli <153308196+Michael-Espineli@users.noreply.github.com>
2024-10-20 23:33:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def hello() -> str:
     return "Hello, Mrs Chris Dean, Welcome to the Party!"
 
 @app.route('/hello')
-def hello():
+def hello()-> str:
     return "Hello"
 # @app.route('/hello/<name>')
 # def hello(name=None):
